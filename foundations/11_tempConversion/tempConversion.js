f7891a04d250e4da1240a559e6d5b6fc8d4e8bd9
Andrei Mago <alexandrei.mago1@gmail.com>
2026-07-02 07:58:50 +0800

    task 11_tempConversion is finished. i incorporated the cleaner code but only as a comment cause i feel like the readability of my code is much more better

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fhn) {
+  
+  let cls= (fhn - 32) * 5/9
+  let result = Math.round(cls *10) / 10
+  cls = result
+  return cls
+// cleaner code: result = Math.round((fhn - 32) * 5/9 * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cls) {
+  let fhn = (cls * 9/5) + 32
+  let result = Math.round(fhn * 10) / 10
+  fhn = result
+  return fhn
+  //cleaner code: result = Math.round((cls * 9/5 + 32) * 10) / 10
 };
 
 // Do not edit below this line
