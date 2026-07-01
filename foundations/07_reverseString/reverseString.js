dd3b481e15eafb82230537a6cde7e21de1c07e97
Andrei Mago <alexandrei.mago1@gmail.com>
2026-07-01 10:49:36 +0800

    Just Finished task 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+let arr = str.split('');
+let reversedArr = arr.reverse();
+let reversedStr = reversedArr.join('');
+return reversedStr;
+    
 };
 
 // Do not edit below this line
